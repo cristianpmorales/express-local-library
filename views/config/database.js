8487f8183a53fe5a19c28a1e75a6e5bd3a8a0536
cristianpmorales <cristianpedromorales@gmail.com>
2018-07-29 00:01:57 -0400

    upload

---
+++ views/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://demo:demo123@ds133041.mlab.com:33041/vending_machine_mo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+            // mongodb://demo:demo123@ds127851.mlab.com:27851/authenticate
+};
